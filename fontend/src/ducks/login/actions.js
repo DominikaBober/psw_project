c4c9211383b89b2e71cdb60a38a74600b67322a6
DominikaBober <75113657+DominikaBober@users.noreply.github.com>
2022-01-30 12:09:14 +0100

    fix actions

--- fontend/src/ducks/login/actions.js
+++ fontend/src/ducks/login/actions.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 
 export const check_if_exists = async (login) => {
-    return  await axios.get(`http://localhost:5000/players/login/${login}`);
+    return await axios.get(`http://localhost:5000/players/login/${login}`)
+        .then((response) => {
+            if (response.status === 200){return response}
+        }).catch((error)=>{
+            console.log(error);
+            return {status: 500}
+        });
 }
 
 export const check_if_good_password = async (values) => {
-    return await axios.post(`http://localhost:5000/players/login`, values);
+    return await axios.post(`http://localhost:5000/players/login`, values)
+        .then((response) => {
+            return response;
+        }).catch((error)=>{
+            console.log(error);
+            return {status: 500};
+        });
 }
 
 export const create_user = async (values) => {
@@ -15,5 +27,31 @@ export const create_user = async (values) => {
         login_date: new Date(),
         role: "player"
     }
-    return await axios.post(`http://localhost:5000/players`, playertoCreate);
+    return await axios.post(`http://localhost:5000/players`, playertoCreate)
+        .then((response) => {
+            return response;
+        }).catch((error)=>{
+            console.log(error);
+            return {status: 500};
+        });
+}
+
+export const get_players = async () => {
+    return await axios.get(`http://localhost:5000/players`)
+        .then((response) => {
+            return response;
+        }).catch((error)=>{
+            console.log(error);
+            return {status: 500};
+        });
+}
+
+export const get_player = async (id) => {
+    return await axios.get(`http://localhost:5000/players/${id}`)
+        .then((response) => {
+            return response;
+        }).catch((error)=>{
+            console.log(error);
+            return {status: 500};
+        });
 }
