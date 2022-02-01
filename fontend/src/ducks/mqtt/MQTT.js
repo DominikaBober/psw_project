3abaed6b371c2b6bb34ffae7a3d10aa61855b2c0
DominikaBober <75113657+DominikaBober@users.noreply.github.com>
2022-02-01 20:03:00 +0100

    mqtt on frontend

--- fontend/src/ducks/mqtt/MQTT.js
+++ fontend/src/ducks/mqtt/MQTT.js
@@ -4,12 +4,12 @@ const Client = mqtt.connect('mqtt://localhost:8000/mqtt');
 
 export const MQTT = () => {
     Client.on('connect', () => {
+        console.log('mqtt conneced');
         Client.subscribe('rules');
-        Client.subscribe('useLogs');
+        Client.subscribe('userLogs');
         Client.subscribe('gameLogs');
         Client.subscribe('chat');
         Client.subscribe('funFacts')
-        console.log('mqtt conneced');
     });
 };
 
