559a797081748a8789e935cf7eb0f784ecba6fbf
DominikaBober <75113657+DominikaBober@users.noreply.github.com>
2022-02-01 23:26:58 +0100

    help functions

--- fontend/src/ducks/game/game.js
+++ fontend/src/ducks/game/game.js
@@ -15,3 +15,21 @@ export const check_results = (game, all_games) => {
                 (el, i) => game.right_text[i] === 0 ? true : game.right_text[i] === el).length
         )
 }
+
+export const create_game = (game, difficulty) => {
+
+    const difficulty_levels = [0.1, 0.35, 0.6, 0.8, 1]
+
+    const bars = {top_text: game.top_text, left_text: game.left_text,
+                                bottom_text: game.bottom_text, right_text: game.right_text}
+
+    const number_of_texts = Math.ceil(difficulty_levels[difficulty-1]*game.top_text.length*4-1)
+
+    for (let i=0; i<number_of_texts; i++){
+        let temp_index = Math.floor(Math.random()*game.top_text.length)
+        let temp_row = Object.keys(bars)[Math.floor(Math.random()*Object.keys(bars).length)]
+        bars[temp_row][temp_index] = 0
+    }
+
+    return bars
+}
