0f1ccdae183d3cf25ddc99c3c9842348fe226be8
DominikaBober <75113657+DominikaBober@users.noreply.github.com>
2022-02-01 20:03:29 +0100

    logs from mqtt

--- database/mqtt_logs.js
+++ database/mqtt_logs.js
@@ -2,11 +2,16 @@ const axios = require('axios');
 const mqtt = require('mqtt');
 
 const Client = mqtt.connect('mqtt://localhost:1883');
+const fun_facts = require('./../fontend/src/ducks/resources.json').fun_facts;
 
 Client.on('connect', () => {
     console.log("connected")
     Client.subscribe('userLogs');
     Client.subscribe('gameLogs');
+    setInterval(() => {
+        let randomId = Math.floor(Math.random() * fun_facts.length);
+        Client.publish('funFacts', fun_facts[randomId])
+    }, 60000)
 });
 Client.on('message', async (topic, message) => {
 
